04c782f7f0e7ae4b1c9a431a119bc587503d05a5
Ponlawat W <ponlawat_w@cmu.ac.th>
2023-04-01 18:11:03 +0700

    To export Characters and segmentExplanation

--- js/src/index.ts
+++ js/src/index.ts
@@ -1,7 +1,7 @@
-import ambiguousDescription from './ambiguous-description';
-import fixOrder from './fix-order';
-import Lexicon from './lexicon';
-import Suggestion from './suggest';
-import textSegment from './text-segment';
-
-export { ambiguousDescription, fixOrder, Lexicon, Suggestion, textSegment };
+export { default as ambiguousDescription } from './ambiguous-description';
+export { default as Characters } from './resources/characters';
+export { default as fixOrder } from './fix-order';
+export { default as Lexicon } from './lexicon';
+export { default as segmentExplanation } from './segment-explanation';
+export { default as Suggestion } from './suggest';
+export { default as textSegment } from './text-segment';
